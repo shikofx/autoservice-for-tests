fe62a379ad2f08f139823d482a0bf53c9039411e
ShikoFX <shikofx@gmail.com>
2019-07-01 21:08:56 +0300

    New example for application

--- autoservice-app-console/src/main/java/com/epam/console/ConsoleManager.java
+++
@@ -1,9 +0,0 @@
-package com.epam.console;
-
-public class ConsoleManager implements IConsoleManager {
-
-    @Override
-    public String readNextLine() {
-        return consoleScanner.nextLine();
-    }
-}
